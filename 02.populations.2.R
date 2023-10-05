c5efb828c01715d23b4f6b977d4232436c99227b
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-05 17:50:47 +0200

    Update 02.populations.2.R

--- 02.populations.2.R
+++ 02.populations.2.R
@@ -1,2 +1,34 @@
 # Code related to population ecology
 
+# A package is needed for point pattern analysis
+install.packages("spatstat")
+library(spatstat)
+
+# let's use the bei data
+# data description:
+# https://CRAN.R-project.org/package=spatstat
+
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=0.5)
+# or even just "cex=.5", so without the "0"
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# let's look at an additional dataset
+bei.extra
+plot(bei.extra)
+# remember that this dataset isn't composed of points, but of maps (images) --> in pratice, raster files
+# one of them is called elev (elevation) and the other one grad (gradient), but we need to use only one part of the data, only "elev"
+
+# let's use only part of the dataset: elev --> we have to use a sort of "rope", the symbol of the $
+plot(bei.extra$elev)
+# of course we can assign this stuff to the object "elevation"
+elevation <- bei.extra$elev
+plot(elevation)
+
