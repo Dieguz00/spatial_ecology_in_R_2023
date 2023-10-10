33a2fc66c4a0cb580bf1bf44b83013cd4de34e34
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-10 17:31:54 +0200

    Update 02.populations.R

--- 02.populations.R
+++ 02.populations.R
@@ -28,6 +28,29 @@ colorRampPalette
 
 cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
 # pay attention: use "" for each colour
-# the "c" means "concatenate
+# the "c" means "concatenate; it stresses that two things are part of the same array
 # "100" is the gradient
 plot(densitymap, col=cl)
+
+# by lowering the gradient, the transition between the different colours becomes sharper
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
+plot(densitymap, col=cl)
+
+# exercise! change the colours without using green, red, or blue together
+crp <- colorRampPalette(c("grey1", "maroon3", "sienna2", "gold"))(100)
+plot(densitymap, col=crp)
+
+#
+plot(bei.extra)
+elev <- bei.extra[[1]]
+# (we use two "[[" as we are dealing with an image (2D))
+plot(elev)
+
+# remember that you could have also used the "$" to recall an element within a dataset: bei.extra$elev
+
+
+# "multiframing" means plotting many things together
+# the function "par" helps us to state how many columns will be present in the plot
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elev)
