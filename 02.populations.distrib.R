ff24b6ad892e43599ecf1adabeea581b4e584691
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-12 17:18:23 +0200

    Update 02.populations.distrib.R

--- 02.populations.distrib.R
+++ 02.populations.distrib.R
@@ -3,7 +3,7 @@
 library(sdm)
 library(terra)
 
-file <- system.file("external/species.shp", packages="sdm")
+file <- system.file("external/species.shp", package="sdm")
 
 rana <- vect(file)
 
@@ -16,3 +16,18 @@ plot(rana)
 plot(rana, cex=.5)
 # the points that you see a could be both absence or presence data
 
+# selecting presences
+rana [rana$Occurrence==1,]
+# because we just selected all the points equal to 1 (presences)
+pres <- rana [rana$Occurrence==1,]
+pres
+pres$Occurrence
+plot(pres, cex=.5)
+
+# exercise: select absences and call the abse
+abse <- rana [rana$Occurrence==0,]
+abse
+abse$Occurrence
+plot(abse, cex=.5)
+
+# you could also use "!=" to pick the things "different from", so the opposite of "=="
