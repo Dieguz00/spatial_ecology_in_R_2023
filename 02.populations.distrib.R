b109f61febeaaae16d166bc116dde8ef96e6ce85
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-12 16:52:58 +0200

    Update 02.populations.distrib.R

--- 02.populations.distrib.R
+++ 02.populations.distrib.R
@@ -9,3 +9,10 @@ rana <- vect(file)
 
 rana$Occurrence
 # and you obtain presence/absence data
+# the "0" is anyaway uncertain, as you could have just missed it, but maybe it was there
+
+# to look at the data
+plot(rana)
+plot(rana, cex=.5)
+# the points that you see a could be both absence or presence data
+
