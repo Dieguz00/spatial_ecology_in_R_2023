5f194e313fce6828f4e084b49b63f74e748bd266
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-19 17:40:53 +0200

    Update 03.communities.overlap.R

--- 03.communities.overlap.R
+++ 03.communities.overlap.R
@@ -5,3 +5,24 @@ library(overlap)
 data(kerinci)
 head(kerinci)
 tail(kerinci)
+
+# to obtain some general information about the dataset
+summary(kerinci)
+
+# to analyse the movement of just one species during the day
+tiger <- kerinci[kerinci$Sps=="tiger",]
+
+# to add the column of "circular time" to the dataset
+kerinci$TimeRad <- kerinci$Time * 2 * pi
+
+# let's relaunch the "tiger", now with the parameter "TimeRad"
+tiger <- kerinci[kerinci$Sps=="tiger",]
+
+# let's name the variable "TimeRad" for "tiger"
+TimeRadTiger <- tiger$TimeRad
+
+# to plot the peaks in time of a certain species
+densityPlot(TimeRadTiger, rug=TRUE)
+# the rug is useful to obtain a smoother curve
+
+# exercise: select only data about macaque
