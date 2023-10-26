05c4423c4fd91668ecebe5caa22477c654e6fd64
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-26 17:43:12 +0200

    Update 04.remote.sensing.visualisation.R

--- 04.remote.sensing.visualisation.R
+++ 04.remote.sensing.visualisation.R
@@ -7,3 +7,17 @@ devtools::install_github("ducciorocchini/imageRy")
 
 library(imageRy)
 library(terra)
+
+# list the data
+im.list()
+
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+# let's plot b2 with a specific colour palette
+clb <- colorRampPalette(c("darkgrey","grey","lightgrey")) (100)
+plot(b2, col=clb)
+
+# exercise: let's do the same with the green band from Sentinel-2 (band 3)
+b3 <- im.import("sentinel.dolomites.b3.tif")
+clg <- colorRampPalette(c("darkgrey","grey","lightgrey")) (100)
+plot(b3, col=clg)
