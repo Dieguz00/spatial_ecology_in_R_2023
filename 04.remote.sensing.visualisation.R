95f2dec863a06fbbc7a14004aa8d9fdc0f5503f3
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-10-26 16:34:26 +0200

    Create 04.remote.sensing.visualisation.R

---
+++ 04.remote.sensing.visualisation.R
@@ -0,0 +1,9 @@
+# This is a script to visualize satellite data
+
+library(devtools) # packages in R are also called libraries
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+
+library(imageRy)
+library(terra)
