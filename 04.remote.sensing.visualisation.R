3a1e278acc6d98772cecd8712f15b9b4bc885efb
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-11-07 16:38:40 +0100

    Update 04.remote.sensing.visualisation.R

--- 04.remote.sensing.visualisation.R
+++ 04.remote.sensing.visualisation.R
@@ -21,3 +21,51 @@ plot(b2, col=clb)
 b3 <- im.import("sentinel.dolomites.b3.tif")
 clg <- colorRampPalette(c("darkgrey","grey","lightgrey")) (100)
 plot(b3, col=clg)
+
+# import the red band from Sentinel-2 (band 4)
+b4 <- im.import("sentinel.dolomites.b4.tif") 
+plot(b4, col=cl)
+
+# import the NIR band from Sentinel-2 (band 8)
+b8 <- im.import("sentinel.dolomites.b8.tif") 
+plot(b8, col=cl)
+
+# multiframe
+par(mfrow=c(2,2))
+plot(b2, col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+# stack images
+stacksent <- c(b2, b3, b4, b8)
+dev.off() # it closes devices
+plot(stacksent, col=cl)
+
+plot(stacksent[[4]], col=cl)
+
+# Exercise: plot in a multiframe the bands with different color ramps
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clg)
+
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clr)
+
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+# RGB space
+# stacksent: 
+# band2 blue element 1, stacksent[[1]] 
+# band3 green element 2, stacksent[[2]]
+# band4 red element 3, stacksent[[3]]
+# band8 nir element 4, stacksent[[4]]
+im.plotRGB(stacksent, r=3, g=2, b=1)
+
+# now let's make a move of "one"
+im.plotRGB(stacksent, r=4, g=3, b=2)
