9010001ce884bd99703969aabb6ff42f383bb92a
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-11-07 17:50:48 +0100

    Update 05.spectral.indices.R

--- 05.spectral.indices.R
+++ 05.spectral.indices.R
@@ -16,3 +16,12 @@ im.plotRGB(m1992, 1, 2, 3)
 # therefore inverting the colours is easier:
 im.plotRGB(m1992, 2, 1, 3)
 im.plotRGB(m1992, 2, 3, 1)
+# what seems bare soil, in this case, it's water (which usually should be black)
+
+# let's look at the 2006 images
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+im.plotRGB(m2006, r=1, g=2, b=3)
+im.plotRGB(m2006, 1, 2, 3)
+im.plotRGB(m2006, 2, 1, 3)
+im.plotRGB(m2006, 2, 3, 1)
+# pay attention that putting the NIF in the blue increases the attention of the viewer (so it's good)
