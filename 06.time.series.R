bdf0798133278236a05478d1a7c3e3091feace30
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-11-14 17:19:37 +0100

    Update 06.time.series.R

--- 06.time.series.R
+++ 06.time.series.R
@@ -22,7 +22,7 @@ dev.off()
 plot(dif)
 
 # let's change the palette
-cldif <- colorRampPalette(c("blue", "white", "red")) (100)  # --> as it is an array, you have to use the "c"
+cldif <- colorRampPalette(c("blue", "white", "red")) (100)  # as it is an array, you have to use the "c"
 plot(dif, col=cldif)
 
 ## Temperatures over Greenland 2000 -> 2015
@@ -30,3 +30,18 @@ plot(dif, col=cldif)
 
 g2000 <- im.import("greenland.2000.tif")
 g2000
+clgr <- colorRampPalette(c("black", "blue", "white", "red")) (100)
+plot(g2000, col = clgr)   # low T in Greenland
+
+# let's import also the other data about 2005, 2010, and 2015
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+# plot just the last one
+plot(g2015, col = clgr)
+
+# let's plot one after the other one
+par(mfrow=c(2,1))
+plot(g2000, col = clgr)
+plot(g2015, col = clgr)   # a lot of ice is clearly being lost
