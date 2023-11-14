c93c5f86bf9064c7044255b41c57831d1b980fae
Dieguz00 <115725589+Dieguz00@users.noreply.github.com>
2023-11-14 16:45:04 +0100

    Update 06.time.series.R

--- 06.time.series.R
+++ 06.time.series.R
@@ -1,1 +1,25 @@
+## Time series analysis
 
+library(imageRy)
+library(terra)
+
+im.list()
+
+# import the data
+EN01 <- im.import("EN_01.png")   # situation in January
+EN13 <- im.import("EN_13.png")   # situation in March
+
+# to make a unique plot
+par(mfrow=c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+# to calculate the difference between two images in the RGB space --> you can make the difference between the Rs, the Gs, and the Bs respectively (so in terms of single elements)
+# using the first element (band) of images
+dif = EN01[[1]] - EN13[[1]]
+dev.off()
+plot(dif)
+
+# let's change the palette
+cldif <- colorRampPalette(c("blue", "white", "red")) (100)  # --> as it is an array, you have to use the "c"
+plot(dif, col=cldif)
